0af08f569525cfda61892db077585e4f66067719
Fenomanu <61010890+Fenomanu@users.noreply.github.com>
2023-11-18 00:32:38 +0100

    puta mierda

--- test/manu_test/MovableObjs.gd
+++ test/manu_test/MovableObjs.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name MovableObjs
 
-enum EMovable {CAT, BLOCK, KEY, DOOR, CONTROL, CABLETOP, CABLELOW}
+enum EMovable {CAT=0, BLOCK=1, KEY=2, DOOR=3, CONTROL=4, CABLETOP=5, CABLELOW=6}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
