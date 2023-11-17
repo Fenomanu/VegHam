e0774dcac4f82d0a785685ff512eaeaca7e5f2f6
Fenomanu <61010890+Fenomanu@users.noreply.github.com>
2023-11-17 20:58:52 +0100

    Nuevos enums

--- test/manu_test/MovableObjs.gd
+++ test/manu_test/MovableObjs.gd
@@ -1,18 +1,21 @@
 extends Node2D
 class_name MovableObjs
 
-enum EMovable {CAT, BLOCK, KEY, DOOR, WIRE, }
+enum EMovable {CAT, BLOCK, KEY, DOOR, CONTROL, CABLETOP, CABLELOW}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
 
 func check_walkable():
+	
 	pass
 
 func move_object(ini_pos, end_pos):
