c3837267cbeef916af32d963d0d5f355e3c5bd8f
Fenomanu <61010890+Fenomanu@users.noreply.github.com>
2023-11-18 16:20:33 +0100

    paused()

--- escenas/niveles/CharSelector.gd
+++ escenas/niveles/CharSelector.gd
@@ -18,38 +18,29 @@ func _process(delta):
 
 func _on_rat_pressed():
 	cam.track(characters[0],3)
-	characters[0].paused = false;
-	#characters[0].pause(false);
+	characters[0].pause(false);
 	buttons[1].button_pressed = false;
-	characters[1].paused = true;
-	#characters[1].pause(true);
+	characters[1].pause(true);
 	buttons[2].button_pressed = false;
-	characters[2].paused = true;
-	#characters[2].pause(true);
+	characters[2].pause(true);
 	pass # Replace with function body.
 
 
 func _on_human_pressed():
 	cam.track(characters[1],2)
-	characters[0].paused = true;
-	#characters[0].pause(true);
+	characters[0].pause(true);
 	buttons[0].button_pressed = false;
-	characters[1].paused = false;
-	#characters[1].pause(false);
+	characters[1].pause(false);
 	buttons[2].button_pressed = false;
-	characters[2].paused = true;
-	#characters[2].pause(true);
+	characters[2].pause(true);
 	pass # Replace with function body.
 
 
 func _on_bird_pressed():
 	cam.track(characters[2], 1.3)
-	characters[0].paused = true;
-	#characters[0].pause(true);
+	characters[0].pause(true);
 	buttons[0].button_pressed = false;
-	characters[1].paused = true;
-	#characters[1].pause(true);
+	characters[1].pause(true);
 	buttons[1].button_pressed = false;
-	characters[2].paused = false;
-	#characters[2].pause(false);
+	characters[2].pause(false);
 	pass # Replace with function body.
