4a5c4aeadd74389ff788227858aa1a62b052b4b1
Carlosroyu <carlosroyu@gmail.com>
2023-11-18 18:32:29 +0100

    uwu

---
+++ scripts/levels/level2.gd
@@ -0,0 +1,24 @@
+extends MovableObjs
+
+
+@export var basura : Sprite2D
+@export var metas : Array[Sprite2D]
+
+
+func _ready():
+	interactables[[0, basura.position]] = {
+		"type": EMovable.BLOCK, 
+		"block": true,
+		"can_interact": [Player.EClass.HUMAN],
+		"sprite": basura
+	}
+	
+	for meta in metas:
+		interactables[[0, meta.position]] = {
+			"type": EMovable.WIN, 
+			"block": true,
+			"can_interact": [Player.EClass.HUMAN],
+			"sprite": meta
+		}
+
+	super._ready()
